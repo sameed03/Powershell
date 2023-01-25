aaafb81ad78a79fa38d630a0fe530c8bc1c889ea
sameed <sameed@mobilabsolutions.com>
2023-01-25 13:45:16 +0100

    first commit

---
+++ vm/initialize_vm.ps1
@@ -0,0 +1,12 @@
+# Set Execution Policy to allow script to run
+Set-ExecutionPolicy Bypass -Scope Process -Force 
+
+# Choco install and Choco Apps
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install googlechrome -y
+choco install putty -y
+choco install notepadplusplus -y
+choco install sysinternals -y
+
+# Disable Firewall for Testing
+Set-NetFirewallProfile -All -Enabled False
